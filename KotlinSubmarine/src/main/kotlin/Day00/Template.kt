d664593796b7eccbdc08fd12ff93d0e6cbe2d4f5
Thomas Krehbiel <tkrehbiel@gmail.com>
2021-12-26 11:36:00 -0500

    initial kotlin framework

---
+++ KotlinSubmarine/src/main/kotlin/Day00/Template.kt
@@ -0,0 +1,18 @@
+package Day00
+
+import Puzzle
+
+// https://adventofcode.com/2021/day/1
+class Template : Puzzle(1, "Funny Title!") {
+    override val puzzleInput = "day01_input.txt"
+    override val testInput = arrayOf("day01_test.txt")
+    override val label1 = ""
+    override val label2 = ""
+
+    override fun part1(input: String): Long {
+        TODO("part 1 not implemented")
+    }
+    override fun part2(input: String): Long {
+        TODO("part 2 not implemented")
+    }
+}
---
+++ KotlinSubmarine/src/main/kotlin/Puzzle.kt
@@ -0,0 +1,70 @@
+// An abstract framework for running puzzles
+abstract class Puzzle(private val day: Int,
+                      private val title: String) {
+    // Label identifying the puzzle output of part 1
+    abstract val label1: String
+    // Label identifying the puzzle output of part 2
+    abstract val label2: String
+    // Main puzzle input file ie. dayXX_input.txt
+    // Set to "" to skip puzzle input for now
+    abstract val puzzleInput: String
+    // One or more puzzle test files
+    abstract val testInput: Array<String>
+
+    // Run puzzles with test input, then real input.
+    fun run() {
+        println("Advent of Code 2021, Day ${this.day} - ${this.title}")
+        println()
+        for (s in testInput) {
+            reset()
+            parts("Test Input ($s)", s)
+        }
+        if (!puzzleInput.isEmpty()) {
+            reset()
+            parts("Puzzle Input ($puzzleInput)", puzzleInput)
+        }
+    }
+
+    // Optional code to reset state between puzzles
+    internal open fun reset() {}
+
+    // Run part 1 and part 2 with a given input file
+    private fun parts(heading: String, input: String) {
+        println(heading)
+        try {
+            println("Part 1: ${part1(getPath(input))} ($label1)")
+        }
+        catch (e: NotImplementedError) {
+            println("Part 1 not implemented")
+        }
+        try {
+            println("Part 2: ${part2(getPath(input))} ($label2)")
+        }
+        catch (e: NotImplementedError) {
+            println("Part 2 not implemented")
+        }
+        println()
+    }
+
+    // Add the right path to an input filename
+    private fun getPath(input: String): String {
+        return "inputs/$input"
+    }
+
+    // TODO: Snapshot visual state in order to create an animated visualization
+    internal fun snapshot(display: List<String>) {
+        if (display.isEmpty()) return
+        // for (s in display) println(s)
+    }
+
+    // A stub for translating internal state into rows of text for visualizations
+    internal open fun visualize() {
+        snapshot(listOf())
+    }
+
+    // Run part 1 and return an answer
+    abstract fun part1(input: String): Long
+
+    // Run part 2 and return an answer
+    abstract fun part2(input: String): Long
+}
