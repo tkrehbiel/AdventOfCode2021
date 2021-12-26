d7f7f3c8cabe330dc990a4ae3a79a6c309dd8055
Thomas Krehbiel <tkrehbiel@gmail.com>
2021-12-26 11:54:46 -0500

    day 8 seven segment search stub

---
+++ KotlinSubmarine/src/main/kotlin/Day08/SevenSegmentSearch.kt
@@ -0,0 +1,22 @@
+package Day08
+
+import Puzzle
+
+fun main() {
+    SevenSegmentSearch().run()
+}
+
+// https://adventofcode.com/2021/day/8
+class SevenSegmentSearch : Puzzle(8, "Seven Segment Search") {
+    override val puzzleInput = "day08_input.txt"
+    override val testInput = arrayOf("day08_test.txt")
+    override val label1 = ""
+    override val label2 = ""
+
+    override fun part1(input: String): Long {
+        TODO("part 1 not implemented")
+    }
+    override fun part2(input: String): Long {
+        TODO("part 2 not implemented")
+    }
+}
