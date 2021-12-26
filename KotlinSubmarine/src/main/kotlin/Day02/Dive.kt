aab78719866b9ec2836d14a5f4efaa4d7f467fb2
Thomas Krehbiel <tkrehbiel@gmail.com>
2021-12-26 11:50:25 -0500

    day 2 dive stub

---
+++ KotlinSubmarine/src/main/kotlin/Day02/Dive.kt
@@ -0,0 +1,22 @@
+package Day02
+
+import Puzzle
+
+fun main() {
+    Dive().run()
+}
+
+// https://adventofcode.com/2021/day/2
+class Dive : Puzzle(2, "Dive!") {
+    override val puzzleInput = "day02_input.txt"
+    override val testInput = arrayOf("day02_test.txt")
+    override val label1 = ""
+    override val label2 = ""
+
+    override fun part1(input: String): Long {
+        TODO("part 1 not implemented")
+    }
+    override fun part2(input: String): Long {
+        TODO("part 2 not implemented")
+    }
+}
